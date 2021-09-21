06f0ed1de8124dc31254e424640b78efc8e75f09
tux <rubenoctavio@itsn.edu.mx>
2021-09-21 01:36:23 -0500

    se agregaron todos los archivos al repositorio

---
+++ android/app/src/main/kotlin/com/example/carrusel_de_imagenes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carrusel_de_imagenes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  List<String> images = [
+    "https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg",
+    "https://s1.1zoom.me/big3/471/Painting_Art_Back_view_Photographer_575380_3840x2400.jpg",
+    "https://i.blogs.es/594843/chrome/450_1000.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Swiper'),
+        ),
+        body: Container(
+          child: _swiper(),
+        ),
+      ),
+    );
+  }
+
+  Widget _swiper() {
+    return Container(
+      width: double.infinity,
+      height: 250.0,
+      child: Swiper(
+        viewportFraction: 0.8,
+        scale: 0.9,
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            images[index],
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: images.length,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
+    );
+  }
+}
